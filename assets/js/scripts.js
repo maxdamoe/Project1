1b3e6bc98e5ab91677239bc3d923fdb3d04d21c9
cffhr99 <83987504+cffhr99@users.noreply.github.com>
2021-07-07 19:36:24 -0400

    update from main

---
+++ assets/js/scripts.js
@@ -0,0 +1,34 @@
+
+
+let moviesLink = window.location.href
+
+console.log(moviesLink)
+
+
+// Send the name of book that the user is looking for to the Books.html file. This is done when the user enters the book title, and then clicks the search button.
+
+const bookSearch = document.getElementById("bookSearch")
+const bookButton = document.getElementById("bookButton")
+
+
+bookButton.addEventListener('click', () => storeBookName())
+
+function storeBookName() {
+
+    localStorage.setItem('bookName', bookSearch.value);
+    return window.location.assign('file:///C:/Users/maxsi/Desktop/Coding/Project1/assets/Books.html');    
+}
+
+
+// Send the name of movie that the user is looking for to the Movies.html file. This is done when the user enters the book title, and then clicks the search button.
+
+const movieSearch = document.getElementById('movieSearch')
+const movieButton = document.getElementById('movieButton')
+
+movieButton.addEventListener('click', () => storeMovieName())
+
+function storeMovieName() {
+    localStorage.setItem('movieName', movieSearch.value);
+    return window.location.assign('file:///C:/Users/maxsi/Desktop/Coding/Project1/Assets/Movies.html')
+}
+
